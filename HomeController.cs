6055c2980fbeb56097e192338973577c6d1cf36a
Mohammad Ismail <49612845+coxismail@users.noreply.github.com>
2021-03-17 07:47:35 +0600

    something removed (Unnecessary)

--- HomeController.cs
+++ HomeController.cs
@@ -13,24 +13,7 @@ namespace UploadExcel.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
 
         public ActionResult ExcelUpload()
         {
